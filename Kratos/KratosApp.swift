b9b8f9180fc9dd7e439e9aeb75c9210f0ab4edd4
Dhruv Bhagavatula <100023273@mvla.net>
2024-06-11 21:14:16 -0700

    Initial Commit

---
+++ Kratos/KratosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KratosApp.swift
+//  Kratos
+//
+//  Created by Dhruv Bhagavatula on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct KratosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
